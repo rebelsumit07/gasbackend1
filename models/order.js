f47b8e78d56de3e3c18ccec84509a13bf3379cb5
rebelsumit07 <rebelsumit636@gmail.com>
2025-10-15 11:24:59 +0530

    order.js

--- models/order.js
+++ models/order.js
@@ -1,8 +1,8 @@
-// models/orderModel.js
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
+    orderId: { type: String, required: true, unique: true }, // ✅ Unique order ID
     customerName: { type: String, required: true },
     customerEmail: { type: String, required: true },
     customerPhone: { type: String, required: true },
